3049ae26494bdfd964577a1cbc106c66bca6ba56
Emilie Ickart <emi.ickart@gmail.com>
2022-07-05 11:16:50 +0200

    Ft-Add icons

--- contact.php
+++ contact.php
@@ -35,7 +35,9 @@ require_once 'layout/header.php' ?>
                     <textarea class="form-control" placeholder="Laissez votre message ici" id="floatingTextarea" name="message" style="height: 200px" required></textarea>
                     <label for="floatingTextarea">Message</label>
                 </div>
-                <button type="submit" class="btn btn-primary">Envoyer</button>
+                <button type="submit" class="btn btn-primary">Envoyer <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#fff" class="bi bi-send" viewBox="0 0 18 18">
+                        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                    </svg></button>
             </div>
         </form>
     </div>
