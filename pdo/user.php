3cc0090f59e62d55367b52f5778838b76be9b4ed
Emilie Ickart <emi.ickart@gmail.com>
2022-07-11 15:17:35 +0200

    update user.php (test file)

--- pdo/user.php
+++ pdo/user.php
@@ -5,25 +5,7 @@ if (!isset($_GET['id'])) {
     exit;
 } // 1ere protection contre l'accès aux informations de la BDD par modification de l'URL
 
-$dbConfig = parse_ini_file('db.ini');
-
-if ($dbConfig === false) {
-    echo 'Erreur : problème de configuration.';
-    exit;
-}
-
-try {
-    $pdo = new PDO(
-        $dbConfig['DB_DSN'],
-        $dbConfig['DB_USER'],
-        $dbConfig['DB_PASSWORD']
-    );
-} catch (PDOException $e) {
-    file_put_contents('dblogs.log', $e->getMessage() . PHP_EOL, FILE_APPEND);
-    exit('Erreur : Connexion à la base de données impossible');
-}
-
-
+require_once 'db_connect.php';
 
 // Récupérer l'ID de l'utilisateur depuis l'URL
 $id = intval($_GET['id']);
